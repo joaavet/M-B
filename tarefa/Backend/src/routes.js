b85d1e10cf97d8500fcb366b001793b8aff437c5
joaavet <jv397803@gmail.com>
2021-08-25 20:29:28 -0300

    3bim  commit

--- tarefa/Backend/src/routes.js
+++ tarefa/Backend/src/routes.js
@@ -3,7 +3,8 @@ const UserController          = require('../controller/User')
 const ClinicaController       = require('../controller/Clinica')
 const EspecialidadeController = require('../controller/Especialidade')
 const MedicoController        = require('../controller/Medico')
-const MedicoClinicaController        = require('../controller/MedicoClinica')
+const MedicoClinicaController = require('../controller/MedicoClinica')
+const MateriaController       = require('../controller/Materia')
 
 const routes = express.Router();
 
@@ -21,6 +22,7 @@ routes.get('/user',UserController.index)
 routes.post('/user',UserController.store)
 routes.put('/user', UserController.update)
 routes.delete('/user', UserController.delete)
+routes.post('/user/validation', UserController.validation)
 
 routes.get('/clinica',ClinicaController.index)
 routes.post('/clinica',ClinicaController.store)
@@ -36,6 +38,12 @@ routes.get('/medico',MedicoController.index)
 routes.post('/medico',MedicoController.store)
 routes.put('/medico', MedicoController.update)
 routes.delete('/medico', MedicoController.delete)
+
+
+routes.get('/materia',MateriaController.index)
+routes.post('/materia',MateriaController.store)
+routes.put('/materia', MateriaController.update)
+routes.delete('/materia', MateriaController.delete)
 
 
 
